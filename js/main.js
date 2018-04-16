ed66db73c5e1e38c7e6ba966cde801329ed03a62
David Sabin <ddansab@localhost.localdomain>
2018-04-15 21:13:07 -0400

    bottom diver

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function () {
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 1500, 'linear');
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 2000, 'linear');
     });
 
     $('.hvr-bounce-to-bottom').click(function () {
