519502ec8cd7139b339095add4f60fa696dc6f43
David Sabin <ddansab@localhost.localdomain>
2018-04-15 22:36:54 -0400

    progress bar

--- js/main.js
+++ js/main.js
@@ -7,4 +7,25 @@ $(function () {
     $('.hvr-bounce-to-bottom').click(function () {
         $(this).unbind("mouseenter mouseleave");
     });
+
+    // progress bar
+    var progressVal = $('progress').val(),
+        progressStep = 20,
+        progressDirection = 1;
+
+    setInterval(function () {
+        console.log(progressDirection, progressVal);
+
+        if (progressDirection > 0 && progressVal < 100) {
+            progressVal += progressStep;
+            $('progress').val(progressVal);
+        } else if (progressDirection < 0 && progressVal > 0) {
+            progressVal -= progressStep;
+            $('progress').val(progressVal);
+        } else if (progressVal == 100) {
+            progressDirection = -1;
+        } else if (progressVal == 0) {
+            progressDirection = 1;
+        }
+    }, 500)
 });
